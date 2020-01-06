1b01a841a37127b7d44f5800434e80660b834810
miraDask <mira.daskalov@gmail.com>
2020-01-06 20:37:38 +0000

    initial

---
+++ QuizHut/QuizHut.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuizHut.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
