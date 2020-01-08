eb8d475f5857a430256c86c62caffc6078b01bb0
miraDask <mira.daskalov@gmail.com>
2020-01-08 00:10:26 +0000

    last progress

--- QuizHut/QuizHut.Web/wwwroot/assets/js/createQuiz.js
+++ QuizHut/QuizHut.Web/wwwroot/assets/js/createQuiz.js
@@ -1,10 +1,9 @@
 ﻿(function () {
 
-    
+    let count = 1;
     let addNameBtn = document.getElementById("addName");
     
     if (addNameBtn) {
-        
 
         addNameBtn.addEventListener("click", addName);
     }
@@ -12,10 +11,28 @@
     function addName(event) {
         event.stopPropagation()
 
+        let nameElement = document.getElementById("quizName");
+        let infoElement = document.getElementById("info");
         let input = document.getElementsByTagName("input")[0];
 
         let name = input.value;
+        nameElement.textContent = name;
+        infoElement.style.display = "none";
 
-        console.log(name);
+        document.getElementById("name").style.display = "none";
+        document.getElementById("question").style.display = "block";
+        document.getElementById("addQuestion").addEventListener("click", addQuestion);
+    }
+
+    function addQuestion(event) {
+        event.stopPropagation();
+        let questionText = document.getElementById("questionText").value;
+
+        displayQuestion(questionText);
+    }
+
+
+    function displayQuestion(text) {
+
     }
 })();
