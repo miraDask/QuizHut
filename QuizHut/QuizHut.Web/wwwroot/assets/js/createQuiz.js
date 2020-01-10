7559aa9f8e8c1ddca0f3e2e0b838b07c7dec7211
miraDask <mira.daskalov@gmail.com>
2020-01-10 14:53:19 +0000

    refactoring createQuiz.js, need fix - to not generate same ids

--- QuizHut/QuizHut.Web/wwwroot/assets/js/createQuiz.js
+++ QuizHut/QuizHut.Web/wwwroot/assets/js/createQuiz.js
@@ -1,4 +1,10 @@
 ﻿(function () {
+
+    const LABEL = {
+        QUESTION: "Question",
+        ANSWER: "Answer",
+        NAME: "Name"
+    };
     
     //prevents Enter key to submit form:
     window.addEventListener('keydown', function (e) {
@@ -14,11 +20,17 @@
     
     let addNameBtn = document.getElementById("addName");
     let addQuestionBtn = document.getElementById("addQuestion");
+    let addAnswerBtn = document.getElementById("addAnswer");
+    let addImageBtn = document.getElementById("addImage");
+    let addTextFieldBtn = document.getElementById("addTextField");
 
     if (addNameBtn) {
 
         addNameBtn.addEventListener("click", addName);
-        addQuestionBtn.addEventListener("click", addQuestion)
+        addQuestionBtn.addEventListener("click", addQuestion);
+        addAnswerBtn.addEventListener("click", addAnswer);
+        addImageBtn.addEventListener("click", addImage);
+        addTextFieldBtn.addEventListener("click", addTextField);
     }
 
     function addName(event) {
@@ -28,29 +40,68 @@
         let quiz = document.getElementById("quiz");
         let text = nameInput.value;
 
-        displayQuizElement(quiz, "Name", text);
-        document.getElementById("nameCard").style.display = "none";
+        let newElement = displayQuizElement(quiz, LABEL.NAME, text);
+        //document.getElementById("nameCard").style.display = "none";
         quiz.style.display = "block";
         let buttons = Array.from(document.getElementsByTagName("button"));
 
         buttons.filter(b => b.classList[0] === "btn")
-               .map(b => b.classList.replace("btn-outline-primary", "btn-primary"));
+            .map(b => b.classList.replace("btn-outline-primary", "btn-primary"));
+
+        window.scroll(0, findPos(newElement));
+
+    }
+
+    function findPos(obj) {
+        var curtop = 0;
+        if (obj.offsetParent) {
+            do {
+                curtop += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+            return [curtop];
+        }
     }
 
     function addQuestion() {
         event.stopPropagation()
-        displayQuizElement(quiz, "Question " + questionCount++, "");
+        let newElement = displayQuizElement(quiz, LABEL.QUESTION, "");
+        window.scroll(0, findPos(newElement));
+    }
+
+    function addAnswer() {
+        event.stopPropagation()
+        let newElement = displayQuizElement(quiz, LABEL.ANSWER, "");
+        window.scroll(0, findPos(newElement));
     }
 
     function displayQuizElement(quiz, labelText, text) {
         let template = document.getElementById("template");
         let element = template.cloneNode(true);
+        let checkbox = element.getElementsByTagName("input")[0].parentNode.parentNode;
+
+        if (labelText === LABEL.NAME) {
+            element.id = labelText.toLowerCase();
+        }
+
+        if (labelText === LABEL.ANSWER) {
+            checkbox.style.display = "block";
+            element.getElementsByTagName("h4")[0].style.display = "none";
+        }
+
+        if (labelText === LABEL.QUESTION) {
+            labelText = labelText + " " + questionCount;
+            element.id = labelText.toLowerCase() + questionCount;
+            questionCount++;
+        }
+
         element.getElementsByTagName("label")[0].textContent = labelText;
         quiz.appendChild(element);
 
-        let input = element.getElementsByTagName("input")[0];
+        let input = element.getElementsByTagName("input")[1];
         input.value = text;
         element.style.display = "block";
+
+        return element;
     }
 
 })();
