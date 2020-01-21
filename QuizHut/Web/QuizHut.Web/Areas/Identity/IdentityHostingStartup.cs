19105a1eb4e4de9e4cfb8d0e658db277cd8f4d03
miraDask <mira.daskalov@gmail.com>
2020-01-21 22:11:54 +0000

    added Identity from scaffold

---
+++ QuizHut/Web/QuizHut.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuizHut.Data;
+using QuizHut.Data.Models;
+
+[assembly: HostingStartup(typeof(QuizHut.Web.Areas.Identity.IdentityHostingStartup))]
+namespace QuizHut.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
