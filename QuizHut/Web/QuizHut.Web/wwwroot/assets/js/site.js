a97a64bec12ac65b603d98703ebc9c3c9a0079b3
miraDask <mira.daskalov@gmail.com>
2020-02-16 20:42:12 +0000

    add read more buttons functionality

---
+++ QuizHut/Web/QuizHut.Web/wwwroot/assets/js/site.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var readMoreBtns = Array.from(document.getElementsByTagName('a')).filter(x => x.classList.contains('read'));
+    $(readMoreBtns).click(loadInfo);
+
+    function loadInfo() {
+        var paragraph = $(this).parent().prev().children().last();
+        var btnText = $(this).text();
+        if (btnText === 'Read More') {
+            $(paragraph).show();
+            $(this).text('Close');
+        } else {
+            $(paragraph).hide();
+            $(this).text('Read More');
+        }
+    }
+})
