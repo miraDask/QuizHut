9322ea80256d7c704f9b1c981b7b16ff6d639575
miraDask <mira.daskalov@gmail.com>
2020-02-16 23:52:32 +0000

    fix logged in nav bar to be active on correct page in site.js

--- QuizHut/Web/QuizHut.Web/wwwroot/assets/js/site.js
+++ QuizHut/Web/QuizHut.Web/wwwroot/assets/js/site.js
@@ -1,6 +1,36 @@
 ﻿$(function () {
-    var readMoreBtns = Array.from(document.getElementsByTagName('a')).filter(x => x.classList.contains('read'));
+    const NAMES = {
+        HOME: 'Home',
+        GROUPS: 'Groups',
+        QUIZZES: 'Quizzes',
+        CATEGORIES: 'Categories',
+        HASH: '#',
+        SLASH: '/',
+        MANAGE: 'Manage'
+    }
+
+    let readMoreBtns = Array.from(document.getElementsByTagName('a')).filter(x => x.classList.contains('read'));
     $(readMoreBtns).click(loadInfo);
+
+    var navLinks = Array.from(document.getElementsByTagName('a')).filter(x => x.classList.contains('header'));
+    if (navLinks) {
+        let pageUrl = window.location.href;
+        if (pageUrl.includes(NAMES.QUIZZES)) {
+            activateNavLink(navLinks[2]);
+        } else if (pageUrl.includes(NAMES.MANAGE)) {
+            activateNavLink(navLinks[4]);
+        } else if (pageUrl.includes(NAMES.CATEGORIES)) {
+            activateNavLink(navLinks[1]);
+        } else if (pageUrl.includes(NAMES.GROUPS)) {
+            activateNavLink(navLinks[3]);
+        } else {
+            activateNavLink(navLinks[0]);
+        }
+    }
+
+    function activateNavLink(element) {
+        element.classList.add('active');
+    }
 
     function loadInfo() {
         var paragraph = $(this).parent().prev().children().last();
