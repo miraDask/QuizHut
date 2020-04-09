7d005d9ed3904f7a76031fbf4b2a3a39fc35ae41
miraDask <mira.daskalov@gmail.com>
2020-04-09 18:25:48 +0100

    refactor StudentsGroupsServiceTests to inherit BaseServiceTests - all tests passed

--- QuizHut/Tests/QuizHut.Services.Data.Tests/StudentsGroupsServiceTests.cs
+++ QuizHut/Tests/QuizHut.Services.Data.Tests/StudentsGroupsServiceTests.cs
@@ -5,27 +5,14 @@
     using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
-    using QuizHut.Data;
+    using Microsoft.Extensions.DependencyInjection;
     using QuizHut.Data.Models;
-    using QuizHut.Data.Repositories;
     using QuizHut.Services.StudentsGroups;
     using Xunit;
 
-    public class StudentsGroupsServiceTests
+    public class StudentsGroupsServiceTests : BaseServiceTests
     {
-        private readonly ApplicationDbContext dbContext;
-        private readonly EfRepository<StudentGroup> repository;
-        private readonly StudentsGroupsService service;
-
-        public StudentsGroupsServiceTests()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-               .UseInMemoryDatabase(Guid.NewGuid().ToString())
-               .Options;
-            this.dbContext = new ApplicationDbContext(options);
-            this.repository = new EfRepository<StudentGroup>(this.dbContext);
-            this.service = new StudentsGroupsService(this.repository);
-        }
+        private IStudentsGroupsService Service => this.ServiceProvider.GetRequiredService<IStudentsGroupsService>();
 
         [Fact]
         public async Task DeleteAsyncShouldDeleteCorrectly()
@@ -34,10 +21,10 @@
             var groupId = await this.CreateGroupAsync();
             await this.CreateStudentGroupAsync(studentId, groupId);
 
-            await this.service.DeleteAsync(groupId, studentId);
+            await this.Service.DeleteAsync(groupId, studentId);
 
-            var studentsGroupsCount = this.dbContext.StudentsGroups.ToArray().Count();
-            var studentGroup = await this.dbContext.StudentsGroups.FindAsync(new string[] { studentId, groupId });
+            var studentsGroupsCount = this.DbContext.StudentsGroups.ToArray().Count();
+            var studentGroup = await this.DbContext.StudentsGroups.FindAsync(new string[] { studentId, groupId });
             Assert.Equal(0, studentsGroupsCount);
             Assert.Null(studentGroup);
         }
@@ -52,7 +39,7 @@
             await this.CreateStudentGroupAsync(firstStudentId, groupId);
             await this.CreateStudentGroupAsync(secondStudentId, groupId);
 
-            var studentIds = await this.service.GetAllStudentsIdsByGroupIdAsync(groupId);
+            var studentIds = await this.Service.GetAllStudentsIdsByGroupIdAsync(groupId);
 
             Assert.Contains(firstStudentId, studentIds);
             Assert.Contains(secondStudentId, studentIds);
@@ -63,9 +50,9 @@
         {
             var groupId = await this.CreateGroupAsync();
             var studentId = await this.CreateStudentAsync();
-            await this.service.CreateStudentGroupAsync(groupId, studentId);
+            await this.Service.CreateStudentGroupAsync(groupId, studentId);
 
-            var studentGroup = await this.dbContext.StudentsGroups.FindAsync(new string[] { studentId, groupId });
+            var studentGroup = await this.DbContext.StudentsGroups.FindAsync(new string[] { studentId, groupId });
 
             Assert.NotNull(studentGroup);
             Assert.Equal(studentId, studentGroup.StudentId);
@@ -77,10 +64,10 @@
         {
             var groupId = await this.CreateGroupAsync();
             var studentId = await this.CreateStudentAsync();
-            await this.service.CreateStudentGroupAsync(groupId, studentId);
-            await this.service.CreateStudentGroupAsync(groupId, studentId);
+            await this.Service.CreateStudentGroupAsync(groupId, studentId);
+            await this.Service.CreateStudentGroupAsync(groupId, studentId);
 
-            var studentsGroupsCount = this.dbContext.StudentsGroups.ToArray().Count();
+            var studentsGroupsCount = this.DbContext.StudentsGroups.ToArray().Count();
 
             Assert.Equal(1, studentsGroupsCount);
         }
@@ -93,9 +80,9 @@
                 GroupId = groupId,
             };
 
-            await this.dbContext.StudentsGroups.AddAsync(studentGroup);
-            await this.dbContext.SaveChangesAsync();
-            this.dbContext.Entry<StudentGroup>(studentGroup).State = EntityState.Detached;
+            await this.DbContext.StudentsGroups.AddAsync(studentGroup);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<StudentGroup>(studentGroup).State = EntityState.Detached;
         }
 
         private async Task<string> CreateStudentAsync()
@@ -108,9 +95,9 @@
                 UserName = "email@email.com",
             };
 
-            await this.dbContext.Users.AddAsync(student);
-            await this.dbContext.SaveChangesAsync();
-            this.dbContext.Entry<ApplicationUser>(student).State = EntityState.Detached;
+            await this.DbContext.Users.AddAsync(student);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<ApplicationUser>(student).State = EntityState.Detached;
             return student.Id;
         }
 
@@ -118,9 +105,9 @@
         {
             var creatorId = Guid.NewGuid().ToString();
             var group = new Group() { Name = "Group", CreatorId = creatorId };
-            await this.dbContext.Groups.AddAsync(group);
-            await this.dbContext.SaveChangesAsync();
-            this.dbContext.Entry<Group>(group).State = EntityState.Detached;
+            await this.DbContext.Groups.AddAsync(group);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<Group>(group).State = EntityState.Detached;
             return group.Id;
         }
     }
