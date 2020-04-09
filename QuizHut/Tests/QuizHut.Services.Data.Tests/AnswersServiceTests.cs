e6df21d5c1792f750e8f8105964c15857f3d10a1
miraDask <mira.daskalov@gmail.com>
2020-04-09 18:07:39 +0100

    refactor AnswersServiceTests to inherit BaseServiceTests - all tests passed

--- QuizHut/Tests/QuizHut.Services.Data.Tests/AnswersServiceTests.cs
+++ QuizHut/Tests/QuizHut.Services.Data.Tests/AnswersServiceTests.cs
@@ -2,56 +2,38 @@
 {
     using System;
     using System.Linq;
-    using System.Reflection;
     using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
-    using QuizHut.Data;
-    using QuizHut.Data.Common.Repositories;
+    using Microsoft.Extensions.DependencyInjection;
     using QuizHut.Data.Models;
-    using QuizHut.Data.Repositories;
     using QuizHut.Services.Answers;
-    using QuizHut.Services.Mapping;
     using QuizHut.Web.ViewModels.Answers;
     using Xunit;
 
-    public class AnswersServiceTests
+    public class AnswersServiceTests : BaseServiceTests
     {
-        private readonly IDeletableEntityRepository<Answer> repository;
-        private readonly AnswersService service;
-        private readonly ApplicationDbContext dbContext;
-        private readonly string firstAnswerId;
-
-        public AnswersServiceTests()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
-            this.dbContext = new ApplicationDbContext(options);
-            this.repository = new EfDeletableEntityRepository<Answer>(this.dbContext);
-            this.service = new AnswersService(this.repository);
-            AutoMapperConfig.RegisterMappings(typeof(AnswerViewModel).GetTypeInfo().Assembly);
-            this.firstAnswerId = this.CreateFirstAnswer();
-        }
+        private IAnswersService Service => this.ServiceProvider.GetRequiredService<IAnswersService>();
 
         [Fact]
         public async Task CreateAnswerAsyncShouldCreateNewAnswerInDb()
         {
             var questionId = Guid.NewGuid().ToString();
+            await this.Service.CreateAnswerAsync(answerText: "answer", isRightAnswer: false, questionId);
 
-            await this.service.CreateAnswerAsync(answerText: "Second answer", isRightAnswer: false, questionId);
-            var secondAnswer = await this.dbContext.Answers.Where(x => x.Id != this.firstAnswerId).FirstOrDefaultAsync();
-            var answersCount = this.dbContext.Answers.ToArray().Count();
-            Assert.Equal(2, answersCount);
-            Assert.Equal("Second answer", secondAnswer.Text);
-            Assert.Equal(questionId, secondAnswer.QuestionId);
-            Assert.False(secondAnswer.IsRightAnswer);
+            var answer = await this.DbContext.Answers.FirstOrDefaultAsync();
+            var answersCount = this.DbContext.Answers.ToArray().Count();
+
+            Assert.Equal(1, answersCount);
+            Assert.Equal("answer", answer.Text);
+            Assert.Equal(questionId, answer.QuestionId);
+            Assert.False(answer.IsRightAnswer);
         }
 
         [Fact]
         public async Task GetByIdAsyncShouldReturnCorrectModel()
         {
-            var answer = await this.dbContext.Answers.FirstOrDefaultAsync(x => x.Id == this.firstAnswerId);
+            var answer = await this.CreateAnswerAsync();
 
             var model = new AnswerViewModel()
             {
@@ -61,7 +43,7 @@
                 IsRightAnswer = answer.IsRightAnswer,
             };
 
-            var resultModel = await this.service.GetByIdAsync<AnswerViewModel>(answer.Id);
+            var resultModel = await this.Service.GetByIdAsync<AnswerViewModel>(answer.Id);
 
             Assert.Equal(model.Id, resultModel.Id);
             Assert.Equal(model.Text, resultModel.Text);
@@ -72,8 +54,10 @@
         [Fact]
         public async Task UpdateAsyncShouldUpdateCorrectly()
         {
-            await this.service.UpdateAsync(this.firstAnswerId, "First test answer", false);
-            var updatedAnswer = await this.dbContext.Answers.FirstOrDefaultAsync(x => x.Id == this.firstAnswerId);
+            var answer = await this.CreateAnswerAsync();
+
+            await this.Service.UpdateAsync(answer.Id, "First test answer", false);
+            var updatedAnswer = await this.DbContext.Answers.FirstOrDefaultAsync(x => x.Id == answer.Id);
 
             Assert.Equal("First test answer", updatedAnswer.Text);
             Assert.False(updatedAnswer.IsRightAnswer);
@@ -82,15 +66,17 @@
         [Fact]
         public async Task DeleteAsyncShouldDeleteCorrectly()
         {
-            await this.service.DeleteAsync(this.firstAnswerId);
+            var answer = await this.CreateAnswerAsync();
 
-            var answersCount = this.dbContext.Answers.Where(x => !x.IsDeleted).ToArray().Count();
-            var answer = await this.dbContext.Answers.FindAsync(this.firstAnswerId);
+            await this.Service.DeleteAsync(answer.Id);
+
+            var answersCount = this.DbContext.Answers.Where(x => !x.IsDeleted).ToArray().Count();
+            var deletedAnswer = await this.DbContext.Answers.FirstOrDefaultAsync(x => x.Id == answer.Id);
             Assert.Equal(0, answersCount);
-            Assert.True(answer.IsDeleted);
+            Assert.Null(deletedAnswer);
         }
 
-        private string CreateFirstAnswer()
+        private async Task<Answer> CreateAnswerAsync()
         {
             var questionId = Guid.NewGuid().ToString();
             var answer = new Answer()
@@ -99,10 +85,10 @@
                 QuestionId = questionId,
                 IsRightAnswer = true,
             };
-            this.dbContext.Answers.Add(answer);
-            this.dbContext.SaveChanges();
-            this.dbContext.Entry<Answer>(answer).State = EntityState.Detached;
-            return answer.Id;
+            await this.DbContext.Answers.AddAsync(answer);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<Answer>(answer).State = EntityState.Detached;
+            return answer;
         }
     }
 }
