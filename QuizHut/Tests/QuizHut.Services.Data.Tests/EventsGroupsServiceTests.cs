ae28d36f5f49726e37d54b8181dfd7afc30d2ad5
miraDask <mira.daskalov@gmail.com>
2020-04-09 18:11:55 +0100

    refactor EventsGroupsServiceTests to inherit BaseServiceTests - all tests passed

--- QuizHut/Tests/QuizHut.Services.Data.Tests/EventsGroupsServiceTests.cs
+++ QuizHut/Tests/QuizHut.Services.Data.Tests/EventsGroupsServiceTests.cs
@@ -5,28 +5,15 @@
     using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
-    using QuizHut.Data;
+    using Microsoft.Extensions.DependencyInjection;
     using QuizHut.Data.Common.Enumerations;
     using QuizHut.Data.Models;
-    using QuizHut.Data.Repositories;
     using QuizHut.Services.EventsGroups;
     using Xunit;
 
-    public class EventsGroupsServiceTests
+    public class EventsGroupsServiceTests : BaseServiceTests
     {
-        private readonly ApplicationDbContext dbContext;
-        private readonly EfDeletableEntityRepository<EventGroup> eventGroupRepository;
-        private readonly EventsGroupsService service;
-
-        public EventsGroupsServiceTests()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
-            this.dbContext = new ApplicationDbContext(options);
-            this.eventGroupRepository = new EfDeletableEntityRepository<EventGroup>(this.dbContext);
-            this.service = new EventsGroupsService(this.eventGroupRepository);
-        }
+        private IEventsGroupsService Service => this.ServiceProvider.GetRequiredService<IEventsGroupsService>();
 
         [Fact]
         public async Task DeleteAsyncShouldDeleteCorrectly()
@@ -35,10 +22,10 @@
             var groupId = await this.CreateGroupAsync();
             await this.CreateEventGroupAsync(groupId, eventId);
 
-            await this.service.DeleteAsync(eventId, groupId);
+            await this.Service.DeleteAsync(eventId, groupId);
 
-            var eventsGroupsCount = this.dbContext.EventsGroups.Where(x => !x.IsDeleted).ToArray().Count();
-            var eventGroup = await this.dbContext.EventsGroups.FindAsync(new string[] { eventId, groupId });
+            var eventsGroupsCount = this.DbContext.EventsGroups.Where(x => !x.IsDeleted).ToArray().Count();
+            var eventGroup = await this.DbContext.EventsGroups.FindAsync(new string[] { eventId, groupId });
             Assert.Equal(0, eventsGroupsCount);
             Assert.True(eventGroup.IsDeleted);
         }
@@ -53,7 +40,7 @@
             await this.CreateEventGroupAsync(firstGroupId, eventId);
             await this.CreateEventGroupAsync(secondGroupId, eventId);
 
-            var groupIds = await this.service.GetAllGroupsIdsByEventIdAsync(eventId);
+            var groupIds = await this.Service.GetAllGroupsIdsByEventIdAsync(eventId);
 
             Assert.Contains(firstGroupId, groupIds);
             Assert.Contains(secondGroupId, groupIds);
@@ -64,8 +51,8 @@
         {
             var eventId = await this.CreateEventAsync();
             var groupId = await this.CreateGroupAsync();
-            await this.service.CreateEventGroupAsync(eventId, groupId);
-            var eventGroup = await this.dbContext.EventsGroups.FindAsync(new string[] { eventId, groupId });
+            await this.Service.CreateEventGroupAsync(eventId, groupId);
+            var eventGroup = await this.DbContext.EventsGroups.FindAsync(new string[] { eventId, groupId });
 
             Assert.NotNull(eventGroup);
             Assert.Equal(eventId, eventGroup.EventId);
@@ -80,8 +67,8 @@
             await this.CreateEventGroupAsync(groupId, eventId);
             await this.DeleteEventGroupAsync(eventId, groupId);
 
-            await this.service.CreateEventGroupAsync(eventId, groupId);
-            var eventGroup = await this.dbContext.EventsGroups.FindAsync(new string[] { eventId, groupId });
+            await this.Service.CreateEventGroupAsync(eventId, groupId);
+            var eventGroup = await this.DbContext.EventsGroups.FindAsync(new string[] { eventId, groupId });
             Assert.NotNull(eventGroup);
             Assert.Equal(eventId, eventGroup.EventId);
             Assert.Equal(groupId, eventGroup.GroupId);
@@ -96,9 +83,9 @@
                 GroupId = groupId,
             };
 
-            await this.dbContext.EventsGroups.AddAsync(eventGroup);
-            await this.dbContext.SaveChangesAsync();
-            this.dbContext.Entry<EventGroup>(eventGroup).State = EntityState.Detached;
+            await this.DbContext.EventsGroups.AddAsync(eventGroup);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<EventGroup>(eventGroup).State = EntityState.Detached;
         }
 
         private async Task<string> CreateEventAsync()
@@ -114,9 +101,9 @@
                 CreatorId = creatorId,
             };
 
-            await this.dbContext.Events.AddAsync(@event);
-            await this.dbContext.SaveChangesAsync();
-            this.dbContext.Entry<Event>(@event).State = EntityState.Detached;
+            await this.DbContext.Events.AddAsync(@event);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<Event>(@event).State = EntityState.Detached;
             return @event.Id;
         }
 
@@ -124,19 +111,19 @@
         {
             var creatorId = Guid.NewGuid().ToString();
             var group = new Group() { Name = "Group", CreatorId = creatorId };
-            await this.dbContext.Groups.AddAsync(group);
-            await this.dbContext.SaveChangesAsync();
-            this.dbContext.Entry<Group>(group).State = EntityState.Detached;
+            await this.DbContext.Groups.AddAsync(group);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<Group>(group).State = EntityState.Detached;
             return group.Id;
         }
 
         private async Task DeleteEventGroupAsync(string eventId, string groupId)
         {
-            var eventGroup = await this.dbContext.EventsGroups.FirstOrDefaultAsync(x => x.EventId == eventId && x.GroupId == groupId);
+            var eventGroup = await this.DbContext.EventsGroups.FirstOrDefaultAsync(x => x.EventId == eventId && x.GroupId == groupId);
             eventGroup.IsDeleted = true;
-            this.dbContext.Update(eventGroup);
-            await this.dbContext.SaveChangesAsync();
-            this.dbContext.Entry<EventGroup>(eventGroup).State = EntityState.Detached;
+            this.DbContext.Update(eventGroup);
+            await this.DbContext.SaveChangesAsync();
+            this.DbContext.Entry<EventGroup>(eventGroup).State = EntityState.Detached;
         }
     }
 }
